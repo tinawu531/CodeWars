351742f1d87780bc4f7c92da9778a8992605028a
tinawu531 <79052289+tinawu531@users.noreply.github.com>
2024-02-06 22:05:24 -0500

    Update 8 kyu On the Canadian Border (SQL for Beginners #2).sql

--- SQL/8 kyu On the Canadian Border (SQL for Beginners #2).sql
+++ SQL/8 kyu On the Canadian Border (SQL for Beginners #2).sql
@@ -15,3 +15,10 @@
 SELECT *
 FROM travelers
 WHERE country NOT IN ('Canada', 'Mexico', 'USA');
+
+-- Another Solution:
+SELECT name, country
+FROM travelers
+WHERE country <> 'USA'
+  AND country <> 'Mexico'
+  AND country <> 'Canada';
